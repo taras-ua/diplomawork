d68d0ff4b5c4c482319c9ac436c6df2ab7057901
taras-ua <tarasrogov@gmail.com>
2015-02-13 21:49:36 +0200

    point urls to static root the hard way

--- diploma/urls.py
+++ diploma/urls.py
@@ -1,13 +1,10 @@
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.conf.urls import patterns, url
 # from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'app.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 
     # url(r'^admin/', include(admin.site.urls)),
 )
-
-urlpatterns += staticfiles_urlpatterns()
